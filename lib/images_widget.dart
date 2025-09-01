0b092299c2c285d78c7e2418f157b53fc5f79f66
citraresa <citraresa1@gmail.com>
2025-09-01 16:49:55 +0700

    update

---
+++ lib/images_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ImagesWidget extends StatelessWidget {
+  const ImagesWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/marsya.jpeg',
+      width: 200,
+      height: 200,
+    );
+  }
+}
